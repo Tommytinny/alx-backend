0822f1c972d61bb53bd3c317a1c6d61aad2a6efb
Tommytinny <tommyteenboi@gmail.com>
2024-11-09 00:24:26 +0100

    fix errors in files

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
-from flask_babel import Babel
+#!/usr/bin/env python3
+"""Flask app integration with babel
+"""
 
-app = Flask(__name__)
+
+from flask import Flask, render_template
+from flask_babel import Babel
 
 
 class Config:
@@ -11,15 +14,18 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTF"
 
-app.config.from_object(Config)
 
+app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
+
 
 @app.route("/", strict_slashes=False)
 def hello_world():
     """Dipslay hello world
     """
     return render_template("1-index.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
