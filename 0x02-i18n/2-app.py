fb86e02138ed393af468ddfa371d2f2a0b23e83d
Tommytinny <tommyteenboi@gmail.com>
2024-11-09 00:37:09 +0100

    fixed pycodestye error in file

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+"""Flask app
+"""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
-
-app = Flask(__name__)
 
 
 class Config:
@@ -9,11 +11,14 @@ class Config:
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTF"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
@@ -21,11 +26,13 @@ def get_locale():
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route("/", strict_slashes=False)
 def hello_world():
     """Dipslay hello world
     """
     return render_template("2-index.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
